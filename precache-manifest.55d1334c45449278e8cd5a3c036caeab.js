82cf747474cc0afca836963f90cce8a9ddb9333b
Vinodh Kanna RJ (DHL IT Services) <vinodh_kanna.rj@dhl.com>
2020-06-24 18:53:02 +0530

    Updates

---
+++ precache-manifest.55d1334c45449278e8cd5a3c036caeab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b813a5c4a18f74dce31ac4279926fb77",
+    "url": "/hacker-news-assignment/index.html"
+  },
+  {
+    "revision": "81e994bede3adb6353b2",
+    "url": "/hacker-news-assignment/static/css/main.efdc47a1.chunk.css"
+  },
+  {
+    "revision": "390e5033b069c7b82501",
+    "url": "/hacker-news-assignment/static/js/2.c8162030.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/hacker-news-assignment/static/js/2.c8162030.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81e994bede3adb6353b2",
+    "url": "/hacker-news-assignment/static/js/main.baa19e75.chunk.js"
+  },
+  {
+    "revision": "165087b8ced7da6c050e",
+    "url": "/hacker-news-assignment/static/js/runtime-main.e560d5f6.js"
+  }
+]);
